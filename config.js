9826642b469d332fe2ec731a8a0ad79c8e53fb4b
Arash847 <100713493+Arash847@users.noreply.github.com>
2025-03-22 18:56:42 +0330

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://Arash847:<db_password>@arbot.1lomg.mongodb.net/?retryWrites=true&w=majority&appName=ARBot",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
